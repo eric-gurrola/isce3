7446b285892333f6d3e5416ff58d9170ba31e210
Bryan Riel <briel@jpl.nasa.gov>
2018-06-11 09:27:46 -0700

    Updated interpolator tests. Check for median absolute deviation.

--- tests/lib/isce/core/interpolator/interpolator.cpp
+++ tests/lib/isce/core/interpolator/interpolator.cpp
@@ -11,12 +11,14 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <valarray>
 #include <vector>
 #include "gtest/gtest.h"
 
 // isce::core
 #include "isce/core/Constants.h"
 #include "isce/core/Interpolator.h"
+#include "isce/core/RectBSpline.h"
 using isce::core::Matrix;
 
 void loadInterpData(Matrix<double> &);
@@ -36,13 +38,46 @@ std::vector<double> arange(double low, double high, double increment) {
     return data;
 }
 
+// Function to compute median of a valarray
+template <typename T>
+T median(std::valarray<T> v) {
+    T value;
+    size_t n = v.size();
+    size_t nmid = v.size() / 2;
+    std::nth_element(std::begin(v), std::begin(v) + nmid, std::end(v));
+    value = v[nmid];
+    if (n % 2 == 1) {
+        return value;
+    }
+    std::nth_element(std::begin(v), std::begin(v) + nmid - 1, std::end(v));
+    value = 0.5 * (value + v[nmid - 1]);
+    return value;
+}
+
+// Function to compute median absolute deviation of a valarray
+template <typename T>
+T medianAbsoluteDeviation(std::valarray<T> v) {
+    // Compute median
+    T medianValue = median(v);
+    // Subtract median and take absolute value
+    for (size_t i = 0; i < v.size(); ++i) {
+        v[i] = std::abs(v[i] - medianValue);
+    }
+    // Compute median of normalized values
+    T mad = median(v);
+    return mad;
+}
+
 struct InterpolatorTest : public ::testing::Test {
 
     // The low resolution data
     isce::core::Matrix<double> M;
     // The low resolution indices
-    std::vector<double> xindex;
-    std::vector<double> yindex;
+    std::valarray<double> xindex;
+    std::valarray<double> yindex;
+    // Spline interpolators
+    isce::core::RectBSpline cubicSpline;
+    isce::core::RectBSpline quinticSpline;
     // Truth data
     isce::core::Matrix<double> true_values;
 
@@ -53,26 +88,40 @@ struct InterpolatorTest : public ::testing::Test {
         InterpolatorTest() {
 
             // Create indices
-            xindex = arange(-5.01, 5.01, 0.25);
-            yindex = arange(-5.01, 5.01, 0.25);
-            size_t nx = xindex.size();
-            size_t ny = yindex.size();
+            auto xvec = arange(-6.01, 6.01, 0.25);
+            auto yvec = arange(-6.01, 6.01, 0.25);
+            size_t nx = xvec.size();
+            size_t ny = yvec.size();
 
-            // Allocate the matrix
+            // Copy to valarrays
+            xindex.resize(nx);
+            yindex.resize(ny);
+            for (size_t i = 0; i < nx; ++i)
+                xindex[i] = xvec[i];
+            for (size_t i = 0; i < ny; ++i)
+                yindex[i] = yvec[i];
+
+            // Allocate the image data for interpolation
             M.resize(ny, nx);
+            std::valarray<double> M_array(ny*nx);
 
             // Fill matrix values with function z = sin(x**2 + y**2)
             for (size_t i = 0; i < ny; ++i) {
                 for (size_t j = 0; j < nx; ++j) {
                     M(i,j) = std::sin(yindex[i]*yindex[i] + xindex[j]*xindex[j]);
+                    M_array[i*nx+j] = M(i,j);
                 }
             }
 
             // Read the truth data
             loadInterpData(true_values);
 
+            // Initialize spline interpolators
+            cubicSpline.initialize(yindex, xindex, M_array, 3, 3, 0.0);
+            quinticSpline.initialize(yindex, xindex, M_array, 5, 5, 0.0);
+
             // Starting coordinate and spacing of data
-            start = -5.01;
+            start = -6.01;
             delta = 0.25;
         }
 };
@@ -80,57 +129,92 @@ struct InterpolatorTest : public ::testing::Test {
 // Test bilinear interpolation
 TEST_F(InterpolatorTest, Bilinear) {
     size_t N_pts = true_values.length();
-    double error = 0.0;
+    std::valarray<double> errors(N_pts);
     for (size_t i = 0; i < N_pts; ++i) {
         // Unpack location to interpolate
         const double x = (true_values(i,0) - start) / delta;
         const double y = (true_values(i,1) - start) / delta;
-        const double zref = true_values(i,2);
+        const double zref = std::sin(true_values(i,0)*true_values(i,0) +
+                                     true_values(i,1)*true_values(i,1));
         // Perform interpolation
         double z = isce::core::Interpolator::bilinear(x, y, M);
-        // Check
-        ASSERT_NEAR(z, zref, 1.0e-8);
         // Accumulate error
-        error += std::pow(z - true_values(i,5), 2);
+        errors[i] = z - zref;
     }
-    ASSERT_TRUE((error / N_pts) < 0.07);
+    ASSERT_TRUE(medianAbsoluteDeviation(errors) < 0.11);
 }
 
 // Test bicubic interpolation
 // Simply test final sum of square errors
 TEST_F(InterpolatorTest, Bicubic) {
     size_t N_pts = true_values.length();
+    std::valarray<double> errors(N_pts);
     double error = 0.0;
     for (size_t i = 0; i < N_pts; ++i) {
         // Unpack location to interpolate
         const double x = (true_values(i,0) - start) / delta;
         const double y = (true_values(i,1) - start) / delta;
-        const double zref = true_values(i,5);
+        const double zref = std::sin(true_values(i,0)*true_values(i,0) +
+                                     true_values(i,1)*true_values(i,1));
         // Perform interpolation
         double z = isce::core::Interpolator::bicubic(x, y, M);
         // Accumulate error
-        error += std::pow(z - zref, 2);
+        errors[i] = z - zref;
     }
-    ASSERT_TRUE((error / N_pts) < 0.058);
+    ASSERT_TRUE(medianAbsoluteDeviation(errors) < 0.04);
 }
 
 // Test biquintic spline interpolation
 TEST_F(InterpolatorTest, Biquintic) {
     size_t N_pts = true_values.length();
-    double error = 0.0;
+    std::valarray<double> errors(N_pts);
     for (size_t i = 0; i < N_pts; ++i) {
         // Unpack location to interpolate
         const double x = (true_values(i,0) - start) / delta;
         const double y = (true_values(i,1) - start) / delta;
-        const double zref = true_values(i,5);
+        const double zref = std::sin(true_values(i,0)*true_values(i,0) + 
+                                     true_values(i,1)*true_values(i,1));
         // Perform interpolation
         double z = isce::core::Interpolator::interp_2d_spline(6, M, x, y);
         // Accumulate error
-        error += std::pow(z - zref, 2);
+        errors[i] = zref - z;
     }
-    ASSERT_TRUE((error / N_pts) < 0.058);
+    ASSERT_TRUE(medianAbsoluteDeviation(errors) < 0.02);
 }
 
+// Test fitpack spline interpolation
+TEST_F(InterpolatorTest, SplineBicubic) {
+    size_t N_pts = true_values.length();
+    std::valarray<double> errors(N_pts);
+    for (size_t i = 0; i < N_pts; ++i) {
+        // Unpack location to interpolate
+        const double x = true_values(i,0);
+        const double y = true_values(i,1);
+        const double zref = std::sin(x*x + y*y);
+        // Perform interpolation
+        double z = cubicSpline.eval(y, x);
+        // Accumulate error
+        errors[i] = z - zref;
+    }
+    ASSERT_TRUE(medianAbsoluteDeviation(errors) < 0.006);
+}
+
+// Test fitpack spline interpolation
+TEST_F(InterpolatorTest, SplineBiquintic) {
+    size_t N_pts = true_values.length();
+    std::valarray<double> errors(N_pts);
+    for (size_t i = 0; i < N_pts; ++i) {
+        // Unpack location to interpolate
+        const double x = true_values(i,0);
+        const double y = true_values(i,1);
+        const double zref = std::sin(x*x + y*y);
+        // Perform interpolation
+        double z = quinticSpline.eval(y, x);
+        // Accumulate error
+        errors[i] = z - zref;
+    }
+    ASSERT_TRUE(medianAbsoluteDeviation(errors) < 0.0007);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
@@ -141,7 +225,7 @@ void loadInterpData(Matrix<double> & M) {
     /*
     Load ground truth interpolation data. The test data is the function:
 
-    z = sqrt(x^2 + y^2)
+    z = sin(x^2 + y^2)
 
     The columns of the data are:
     x_index    y_index    bilinear_interp    bicubic_interp    5thorder_spline    truth
@@ -154,7 +238,7 @@ void loadInterpData(Matrix<double> & M) {
         std::cout << "Error: Failed to open data file for interpolator test." << std::endl;
     }
 
-    std::vector<double> xvec, yvec, zlinear_vec, zcubic_vec, zquintic_vec, ztrue_vec;
+    std::vector<double> xvec, yvec;
 
     // Loop over interpolation data
     while (fid) {
@@ -162,21 +246,17 @@ void loadInterpData(Matrix<double> & M) {
         // Parse line
         std::string str;
         std::stringstream stream;
-        double x, y, z_linear, z_cubic, z_quintic, z_true;
+        double x, y;
 
         std::getline(fid, str);
         if (str.length() < 1)
             break;
         stream << str;
-        stream >> x >> y >> z_linear >> z_cubic >> z_quintic >> z_true;
+        stream >> x >> y;
 
         // Add data to orbit
         xvec.push_back(x);
         yvec.push_back(y);
-        zlinear_vec.push_back(z_linear);
-        zcubic_vec.push_back(z_cubic);
-        zquintic_vec.push_back(z_quintic);
-        ztrue_vec.push_back(z_true);
     }
 
     // Close the file
@@ -184,14 +264,10 @@ void loadInterpData(Matrix<double> & M) {
 
     // Fill the matrix
     const size_t N = xvec.size();
-    M.resize(N, 6);
+    M.resize(N, 2);
     for (size_t i = 0; i < N; ++i) {
         M(i,0) = xvec[i];
         M(i,1) = yvec[i];
-        M(i,2) = zlinear_vec[i];
-        M(i,3) = zcubic_vec[i];
-        M(i,4) = zquintic_vec[i];
-        M(i,5) = ztrue_vec[i];
     }
 }
 
