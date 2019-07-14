0057f954e1aa846342cb8faa98c499c371c3e49c
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-07-14 14:40:12 +0300

    mm: added support for building the {isce3} python package and its extension; there are still unresolved issues: the resting place for the extension module is missing, the cython extension needs access to {CyError.h}, and perhaps others

--- .mm/isce.mm
+++ .mm/isce.mm
@@ -10,20 +10,34 @@ isce.major := 3
 isce.minor := 0
 
 # isce consists of python packages
-isce.packages :=
+isce.packages := isce.pkg
 # libraries
 isce.libraries := isce.lib cereal.lib
 # python extensions
-isce.extensions :=
+isce.extensions := isce.ext
 # and test suites
 isce.tests :=
+
+# the isce python package
+isce.pkg.name := isce3
+isce.pkg.root := packages/isce3/
+isce.pkg.meta :=
+isce.pkg.bin :=
+isce.pkg.ext := extensions/
 
 # the isce lib meta-data
 isce.lib.root := lib/isce/
 isce.lib.stem := isce-$(isce.major).$(isce.minor)
-isce.lib.extern := gdal mpi hdf5 fftw pyre
+isce.lib.extern := gdal hdf5 mpi fftw pyre
 isce.lib.prerequisites := cereal.lib
 isce.lib.c++.flags += $($(compiler.c++).std.c++17)
+
+# the isce extension
+isce.ext.wraps := isce.lib
+isce.ext.root := extensions/cython/isce/
+isce.ext.module := isceextension
+isce.ext.extern := python
+isce.ext.capsule :=
 
 # the cereal lib meta-data
 cereal.lib.root := contrib/cereal/include/cereal/
